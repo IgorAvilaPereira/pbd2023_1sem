803b4bddc53c6c5da35ea9e3a216baaf4aa12f63
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-02-16 14:04:41 -0300

    hello world spark

--- hello-world-spring/src/main/java/com/example/demo/DemoApplication.java
+++ hello-world-spring/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,19 @@
 package com.example.demo;
 
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);        
+        SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/hello")
@@ -19,4 +21,9 @@ public class DemoApplication {
         return String.format("Hello %s!", name);
     }
 
+    @GetMapping("/teste")
+    public ModelAndView teste(Map<String, Object> model) {
+        model.put("mensagem", "ola");
+        return new ModelAndView("teste", model);
+    }
 }
